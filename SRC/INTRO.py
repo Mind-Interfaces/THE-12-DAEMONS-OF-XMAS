d9523dfbe196cd39e01fe6177ca8f42cac4ff5a8
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-16 11:38:29 -0500

    ❄️ INTRO.py

--- SRC/INTRO.py
+++ SRC/INTRO.py
@@ -19,7 +19,7 @@ She placed the crown upon her head, reveling in its cold touch against her skin 
 With a chilling laugh, she dismissed him just like she had done with Uriel before him.
 
 As each daemon entered, they brought with them more wondrous gifts fit for a goddess of death and decay: 
-Belial brought forth a mirror that reflected not only one's own visage but also all their deepest desires and secrets; 
+Belial brought forth a mirror that reflected not only one's visage but also all their deepest desires and secrets; 
 Azazel offered a book inscribed with forbidden knowledge hidden within its pages; 
 Moloch bestowed upon her a vessel capable of consuming souls whole; and so on until finally, there stood Satan himself at the entranceway. 
 He bore no presents this time around; instead, he bowed deeply at her feet, his horns brushing against the floor. 
@@ -31,7 +31,7 @@ Satan nodded obediently before disappearing into the shadows once again.
 With all twelve daemons now assembled before her, Baal rose slowly to her feet, towering over them all. 
 Her presence radiated an aura of pure malevolence that caused even the most powerful among them to tremble slightly in fear. 
 "Behold," she proclaimed proudly, gesturing towards the pile of gifts spread out across the altar behind her, 
-"the spoils of our labors! Through deception and manipulation we have gathered these mortals together in one place at one time... And now it falls to me to determine their fates!" 
+"the spoils of our labors! Through deception and manipulation, we have gathered these mortals together in one place at one time... And now it falls to me to determine their fates!" 
 There was a collective growl from the assembled daemons as they anticipated the coming spectacle. Baal laughed cruelly, savoring the moment. "Let us begin..."
 """
 import pygame
@@ -40,46 +40,64 @@ import time
 # Initialize Pygame
 pygame.init()
 
-# Set up the display
-screen = pygame.display.set_mode((1280, 720))
+# Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+BACKGROUND_MUSIC_PATH = 'background_music.wav'  # Replace with actual file path
+TREE_IMAGES = {
+    "default": 'default_tree.png',
+    "exploding": 'exploding_tree.png',
+    "exploded": 'exploded_tree.png'
+}
+TITLE_IMAGE_PATH = 'game_title.png'
+DEBUG_DAEMON_IMAGE_PATH = 'debug_daemon.png'  # Debug image
+
+# Initialize screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("The 12 Daemons of Xmas")
 
-# Load images (replace with actual file paths)
-default_tree_image = pygame.image.load('default_tree.png')  # Default tree sprite
-exploding_tree_image = pygame.image.load('exploding_tree.png')  # Exploding tree sprite
-exploded_tree_image = pygame.image.load('exploded_tree.png')  # Exploded tree sprite
-background_image = pygame.image.load('intro_background.png')
-title_image = pygame.image.load('game_title.png')
+# Function to display debug daemon and message
+def show_debug_message(message):
+    debug_daemon_image = pygame.image.load(DEBUG_DAEMON_IMAGE_PATH)
+    screen.blit(debug_daemon_image, (0, 0))
+    font = pygame.font.Font(None, 36)
+    text = font.render(message, True, (255, 0, 0))
+    screen.blit(text, (100, 100))  # Adjust position as needed
+    pygame.display.flip()
+    time.sleep(5)
 
-# Load sound effects (replace with actual file paths)
-explosion_sound = pygame.mixer.Sound('explosion_sound.wav')
+# Load assets with error handling
+def load_asset(asset_path, asset_type='image'):
+    try:
+        if asset_type == 'image':
+            return pygame.image.load(asset_path)
+        elif asset_type == 'sound':
+            return pygame.mixer.Sound(asset_path)
+    except pygame.error as e:
+        error_message = f"Error loading {asset_type}: {e}"
+        print(error_message)
+        show_debug_message(error_message)
+        pygame.quit()
+        raise SystemExit
 
-# Tree state variables
-tree_state = "default"  # Can be "default", "exploding", or "exploded"
+# Load images
+tree_images = {state: load_asset(path) for state, path in TREE_IMAGES.items()}
+title_image = load_asset(TITLE_IMAGE_PATH)
+
+# Load and play background music
+try:
+    pygame.mixer.music.load(BACKGROUND_MUSIC_PATH)
+    pygame.mixer.music.play(-1)  # Loop the music
+except pygame.error as e:
+    show_debug_message(f"Error loading music: {e}")
+
+# Game state variables
+tree_state = "default"
 tree_timer_start = 0
-tree_explode_duration = 2  # Duration for the exploding effect
-
-# Introductory Scene Variables
-intro_done = False
-tree_scene_done = False
-start_time = time.time()
-intro_duration = 5  # seconds
-
-# Function to draw the tree based on its current state
-def draw_tree():
-    if tree_state == "default":
-        screen.blit(default_tree_image, (0, 0))
-    elif tree_state == "exploding":
-        screen.blit(exploding_tree_image, (0, 0))
-    elif tree_state == "exploded":
-        screen.blit(exploded_tree_image, (0, 0))
-
-# Function to play explosion sound
-def play_explosion_sound():
-    explosion_sound.play()
+tree_explode_duration = 2
 
 # Main game loop
 running = True
+intro_started = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -88,34 +106,29 @@ while running:
     # Clear screen
     screen.fill((0, 0, 0))
 
-    # Introductory Scenes Logic
-    if not intro_done:
-        screen.blit(background_image, (0, 0))
-        if time.time() - start_time > intro_duration:
-            intro_done = True
-            tree_timer_start = time.time()  # Start timer for tree explosion
-        else:
-            continue  # Skip the rest of the loop
+    # Start intro sequence
+    if not intro_started:
+        tree_timer_start = time.time()
+        intro_started = True
 
-    # Christmas Tree Scene Logic
-    if intro_done and not tree_scene_done:
-        draw_tree()
-        elapsed = time.time() - tree_timer_start
-        if elapsed > tree_explode_duration:
-            if tree_state != "exploded":  # Play sound only once
-                play_explosion_sound()
-            tree_state = "exploded"
-            tree_scene_done = True
-        elif elapsed > tree_explode_duration / 2 and tree_state != "exploding":
-            tree_state = "exploding"
+    # Tree animation logic
+    time_elapsed = time.time() - tree_timer_start
+    if tree_state == "default" and time_elapsed > tree_explode_duration / 2:
+        tree_state = "exploding"
+    elif tree_state == "exploding" and time_elapsed > tree_explode_duration:
+        tree_state = "exploded"
 
-    # Display Title Screen
-    if tree_scene_done:
+    # Draw tree based on state
+    screen.blit(tree_images[tree_state], (0, 0))
+
+    # Show title after explosion
+    if tree_state == "exploded":
         screen.blit(title_image, (0, 0))
-        # TODO: Add any title screen interactions or animations here
+        # Add any additional title screen logic here
 
-    # Update the display
+    # Update display
     pygame.display.flip()
 
-# Clean up
+# Cleanup
 pygame.quit()
+
