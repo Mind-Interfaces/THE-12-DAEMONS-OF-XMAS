009c061b438d894370f0a5d4f48e2a20dfdc1ebe
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-16 13:59:14 -0500

    ❄️ LVL-04.py

--- SRC/LVL-04.py
+++ SRC/LVL-04.py
@@ -16,7 +16,9 @@ Collecting each mask bestows upon Lilith powerful abilities but also brings her 
 Ultimately, she must face off against Lucifuge herself in a climactic dance of life and death, where the fate of not only herself but also everyone else present hangs in the balance.
 """
 import pygame
+import sys
 import random
+import time
 
 # Initialize Pygame
 pygame.init()
@@ -26,15 +28,42 @@ width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Lucifuge's Hide-and-Seek")
 
-# Load images (replace with your own assets)
-lilith_image = pygame.image.load('lilith_sprite.png')
+# Load images and sounds
+def load_image(image_path):
+    try:
+        return pygame.image.load(image_path)
+    except pygame.error as e:
+        show_debug_message(f"Error loading image {image_path}: {e}")
+        return None
+
+def load_sound(sound_path):
+    try:
+        return pygame.mixer.Sound(sound_path)
+    except pygame.error as e:
+        show_debug_message(f"Error loading sound {sound_path}: {e}")
+        return None
+
+# Debug Daemon for error handling
+def show_debug_message(message):
+    debug_daemon_image = load_image('path/to/debug_daemon.png')
+    screen.blit(debug_daemon_image, (50, 50))
+    text = font.render(message, True, (255, 0, 0))
+    screen.blit(text, (100, 150))
+    pygame.display.flip()
+    time.sleep(5)
+
+# Game assets
+lilith_image = load_image('path/to/lilith_sprite.png')
 mask_images = {
-    'Famine': pygame.image.load('famine_mask.png'),
-    'War': pygame.image.load('war_mask.png'),
-    'Pestilence': pygame.image.load('pestilence_mask.png'),
-    'Death': pygame.image.load('death_mask.png')
+    'Famine': load_image('path/to/famine_mask.png'),
+    'War': load_image('path/to/war_mask.png'),
+    'Pestilence': load_image('path/to/pestilence_mask.png'),
+    'Death': load_image('path/to/death_mask.png')
 }
-background_image = pygame.image.load('tower_background.png')
+background_image = load_image('path/to/tower_background.png')
+background_music = load_sound('path/to/background_music.ogg')
+mask_pickup_sound = load_sound('path/to/mask_pickup_sound.ogg')
+final_showdown_music = load_sound('path/to/final_showdown_music.ogg')
 
 # Game variables
 lilith_position = [width // 2, height // 2]
@@ -44,18 +73,8 @@ collected_masks = []
 # Font for displaying text
 font = pygame.font.Font(None, 36)
 
-def draw_text(text, position):
-    text_surface = font.render(text, True, (255, 255, 255))
-    screen.blit(text_surface, position)
-
-# Main game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Player movement
+# Player Movement Function
+def move_lilith():
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         lilith_position[0] -= 5
@@ -66,23 +85,45 @@ while running:
     if keys[pygame.K_DOWN]:
         lilith_position[1] += 5
 
-    # Mask collection logic
+# Mask Collection Logic
+def collect_masks():
     for name, position in mask_positions.items():
         if pygame.Rect(lilith_position[0], lilith_position[1], lilith_image.get_width(), lilith_image.get_height()).colliderect(
             pygame.Rect(position[0], position[1], mask_images[name].get_width(), mask_images[name].get_height())):
             collected_masks.append(name)
             del mask_positions[name]
+            mask_pickup_sound.play()
 
-    # Draw everything
+# Drawing Game Elements
+def draw_game_elements():
     screen.blit(background_image, (0, 0))
     for name, position in mask_positions.items():
         screen.blit(mask_images[name], position)
     screen.blit(lilith_image, lilith_position)
-
-    # Display collected masks
     draw_text(f"Masks Collected: {', '.join(collected_masks)}", (10, 10))
 
+# Final Showdown with Lucifuge
+def final_showdown():
+    final_showdown_music.play()
+    # Logic for the final showdown with Lucifuge
+
+# Main Game Loop
+background_music.play(-1)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            move_lilith()
+
+    collect_masks()
+    if len(collected_masks) == len(mask_images):
+        final_showdown()
+
+    screen.fill(BLACK)
+    draw_game_elements()
     pygame.display.flip()
 
 pygame.quit()
-
+sys.exit()
