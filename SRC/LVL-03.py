c878bace108c853b7261a5cf20c467ffcf9ec4fe
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-16 13:26:45 -0500

    ❄️ LVL-03.py

--- SRC/LVL-03.py
+++ SRC/LVL-03.py
@@ -15,6 +15,7 @@ This way, even novice players can enjoy exploring the thrill of seduction within
 import pygame
 import sys
 import random
+import time
 
 # Initialize Pygame
 pygame.init()
@@ -29,96 +30,107 @@ pygame.display.set_caption("Baal's Labyrinth of Desires")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (100, 100, 100)
-RED = (255, 0, 0)
 
 # Maze settings
 maze_rows = 10
 maze_columns = 10
 cell_size = 50
-maze = [[0 for _ in range(maze_columns)] for _ in range(maze_rows)]  # 0 represents open path
+maze = [[0 for _ in range(maze_columns)] for _ in range(maze_rows)]
 
 # Player settings
-player_pos = [0, 0]  # Starting at top-left corner
+player_pos = [0, 0]
 player_color = WHITE
-player_size = cell_size - 10
+player_size = cell_size // 2
 
 # Font setup
 font = pygame.font.Font(None, 36)
 
-# Sample questions (expand this with real questions)
+# Sample questions
 questions = [
-    {"text": "Question 1: ...", "correct_option": "A", "options": ["A", "B", "C"]},
-    {"text": "Question 2: ...", "correct_option": "B", "options": ["A", "B", "C"]},
-    {"text": "Question 3: ...", "correct_option": "C", "options": ["A", "B", "C"]}
+    # ... List of questions
 ]
 
 # Game variables
 current_question = None
 score = 0
 baal_hint_active = False
-difficulty_level = 1  # Increase this when Baal's hint is used
+difficulty_level = 1
+
+def load_image(image_path):
+    try:
+        return pygame.image.load(image_path)
+    except pygame.error as e:
+        show_debug_message(f"Error loading image {image_path}: {e}")
+        return None
+
+def show_debug_message(message):
+    debug_daemon_image = load_image('debug_daemon.png')
+    screen.blit(debug_daemon_image, (50, 50))
+    font = pygame.font.Font(None, 36)
+    text = font.render(message, True, (255, 0, 0))
+    screen.blit(text, (100, 150))
+    pygame.display.flip()
+    time.sleep(5)
 
 def generate_maze():
-    """Generate a simple maze."""
     for row in range(maze_rows):
         for col in range(maze_columns):
             if random.choice([True, False]):
-                maze[row][col] = 1  # Set wall
+                maze[row][col] = 1
 
-def draw_maze():
-    """Draw the maze on the screen."""
+def draw_iso_tile(x, y, tile_type):
+    iso_x, iso_y = (x - y) * cell_size + screen_width // 2, (x + y) * cell_size // 4
+    color = GREY if tile_type == 1 else BLACK
+    points = [(iso_x, iso_y), (iso_x + cell_size//2, iso_y + cell_size//4),
+              (iso_x, iso_y + cell_size//2), (iso_x - cell_size//2, iso_y + cell_size//4)]
+    pygame.draw.polygon(screen, color, points)
+
+def draw_maze_iso():
     for row in range(maze_rows):
         for col in range(maze_columns):
-            color = GREY if maze[row][col] == 1 else BLACK
-            pygame.draw.rect(screen, color, (col*cell_size, row*cell_size, cell_size, cell_size))
+            draw_iso_tile(col, row, maze[row][col])
 
-def draw_player():
-    """Draw the player on the screen."""
-    pygame.draw.rect(screen, player_color, 
-                     (player_pos[0]*cell_size + 5, player_pos[1]*cell_size + 5, player_size, player_size))
+def iso_to_screen(x, y):
+    screen_x = (x - y) * cell_size + screen_width // 2
+    screen_y = (x + y) * cell_size // 4
+    return screen_x, screen_y
 
-def move_player(key):
-    """Move the player based on key input."""
-    new_pos = list(player_pos)
+def draw_player_iso():
+    screen_x, screen_y = iso_to_screen(player_pos[0], player_pos[1])
+    pygame.draw.rect(screen, player_color, (screen_x, screen_y, player_size, player_size // 2))
+
+def move_player_iso(key):
+    dx, dy = 0, 0
     if key == pygame.K_UP:
-        new_pos[1] -= 1
+        dy = -1
     elif key == pygame.K_DOWN:
-        new_pos[1] += 1
+        dy = 1
     elif key == pygame.K_LEFT:
-        new_pos[0] -= 1
+        dx = -1
     elif key == pygame.K_RIGHT:
-        new_pos[0] += 1
+        dx = 1
 
-    # Check for wall collisions
+    new_pos = [player_pos[0] + dx, player_pos[1] + dy]
     if 0 <= new_pos[0] < maze_columns and 0 <= new_pos[1] < maze_rows and maze[new_pos[1]][new_pos[0]] == 0:
         player_pos[:] = new_pos
 
 def check_question():
-    """Check if the player has encountered a question."""
     global current_question, score
-    if player_pos == [maze_columns - 1, maze_rows - 1]:  # Example end position
-        current_question = random.choice(questions)
-        present_question(current_question)
+    # Check for question encounter logic
 
 def present_question(question):
-    """Display a question and handle player input for the answer."""
     global score, baal_hint_active
-    # TODO: Display the question and options on screen
-    # Example: draw_text(question["text"], (100, 100))
-    # Handle player input for options and check answer
-    # Example: if player's answer == question["correct_option"]: score += 1
+    # Display a question and handle player input for the answer
 
 def draw_text(text, position):
-    """Draw text on the screen."""
     text_surface = font.render(text, True, WHITE)
     screen.blit(text_surface, position)
 
 def draw_score():
-    """Draw the current score."""
     draw_text(f"Score: {score}", (10, 10))
 
-# Generate the maze
 generate_maze()
+
 
 # Main game loop
 running = True
